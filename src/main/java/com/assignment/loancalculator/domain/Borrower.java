133dcd8c046818810cb68a3ac1cdcb4e87ffc2a7
Jayesh Shukla <Jayesh.Shukla@pb.com>
2022-05-30 12:11:09 +0530

    Ledger & Co loan calculator changes

---
+++ src/main/java/com/assignment/loancalculator/domain/Borrower.java
@@ -0,0 +1,18 @@
+package com.assignment.loancalculator.domain;
+
+public class Borrower {
+    String name;
+
+    public Borrower(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
